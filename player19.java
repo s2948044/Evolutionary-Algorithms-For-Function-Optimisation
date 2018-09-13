9e040f4134a8457beafab57f3ce948ed60e99eb2
Vincent Wen <x.wen@student.tue.nl>
2018-09-13 15:21:02 +0200

    Added rouletteWheelSelection method.

--- player19.java
+++ player19.java
@@ -49,12 +49,44 @@ public class player19 implements ContestSubmission {
         while (evals < evaluations_limit_) {
             // Select parents
             // Apply crossover / mutation operators
-            double child[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+            // double child[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+            double child[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             // Check fitness of unknown fuction
             Double fitness = (double) evaluation_.evaluate(child);
+            System.out.println("Selected " + rouletteWheelSelection(child));
             evals++;
             // Select survivors
         }
 
+    }
+
+    /**
+     * Roulette Wheel Selection.
+     * 
+     * @param fitnessValues Array of fitness values corresponding to the population.
+     * @return Index of the selected individual.
+     */
+    private int rouletteWheelSelection(double[] fitnessValues) {
+        // Sum up the fitnessValues of all the selectees.
+        double fitnessSum = 0;
+        for (double fitness : fitnessValues) {
+            fitnessSum += fitness;
+        }
+
+        // Compute the corresponding probabilities.
+        double[] probs = new double[fitnessValues.length];
+        for (int i = 0; i < probs.length; i++) {
+            probs[i] = fitnessValues[i] / fitnessSum;
+        }
+
+        // Init a random probability and return its corresponding index.
+        double prob = new Random().nextDouble();
+        double probSum = 0;
+        int i = 0;
+        while (prob - probSum > 0) {
+            probSum += probs[i];
+            i++;
+        }
+        return i - 1;
     }
 }
