73d21f6452349a0c9e2f6b96f4c8a90d6a270805
Vincent Wen <x.wen@student.tue.nl>
2018-09-19 16:23:06 +0200

    Additional classes.

---
+++ Selections.java
@@ -0,0 +1,9 @@
+public class Selections {
+    public Selections() {
+        System.out.println("Managed to create Selections class.");
+    }
+
+    public void test_print() {
+        System.out.println("Managed to invoke test_print() method from the Selections class.");
+    }
+}
---
+++ Variations.java
@@ -0,0 +1,9 @@
+public class Variations {
+    public Variations() {
+        System.out.println("Managed to create Variations class.");
+    }
+
+    public void test_print() {
+        System.out.println("Managed to invoke test_print() method from the Variations class.");
+    }
+}
