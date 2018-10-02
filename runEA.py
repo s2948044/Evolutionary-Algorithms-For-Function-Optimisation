ff5ccbca581357662f931941d70fdf6bb9826d9e
DirkZomerdijk <dirkzomerdijk@gmail.com>
2018-10-02 22:09:23 +0200

    python commandline
    python script whichs runs EA and plots result using matplotlib

--- runEA.py
+++ runEA.py
@@ -1,32 +1,22 @@
 import os
 import subprocess
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
+import numpy as np
+import matplotlib.pyplot as plt
 
-subprocess.run("makeme")
+# subprocess.run("makeme")
 result = subprocess.run(['java','-jar','testrun.jar','-submission=player19','-evaluation=BentCigarFunction', '-seed=1'], stdout=subprocess.PIPE)
 
 string_array = result.stdout[:-43].decode('utf-8')
 
 string_array = string_array.split(',')
 
-dataset = [];
+dataset = []
 for i in range(len(string_array)):
-
 	dataset.append(float(string_array[i]))
 
-print(dataset)
-
-# Create a trace
-trace = go.Scatter(
-    x = [range(len(dataset))],
-    y = random_y
-)
-
-data = [trace]
-
-py.iplot(data, filename='basic-line')
+x = [i for i in range(len(dataset)) ]
+plt.plot(x,dataset)
+plt.show()
 
 
 
