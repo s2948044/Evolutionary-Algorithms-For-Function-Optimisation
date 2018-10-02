4fe47485f72493e71ab1d1fe97e6aa4a633cf94f
DirkZomerdijk <dirkzomerdijk@gmail.com>
2018-10-02 21:40:50 +0200

    mannus check deze

--- runEA.py
+++ runEA.py
@@ -1,24 +1,32 @@
 import os
-
-# os.system("makeme")
-
-# filedir = "data/test.csv"
-# string = "java -jar testrun.jar -submission=player19 -evaluation=BentCigarFunction -seed=1 > " + filedir
-# os.system(str(string))
-
 import subprocess
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+subprocess.run("makeme")
 result = subprocess.run(['java','-jar','testrun.jar','-submission=player19','-evaluation=BentCigarFunction', '-seed=1'], stdout=subprocess.PIPE)
 
 string_array = result.stdout[:-43].decode('utf-8')
 
 string_array = string_array.split(',')
 
-# print(string_array)
-datap = []
-for i in range(0,len(string_array),2):
-	# if((i+1)%2 == 0):
-	# 		datap = []
-	# datap.append(string_array[])
+dataset = [];
+for i in range(len(string_array)):
+
+	dataset.append(float(string_array[i]))
+
+print(dataset)
+
+# Create a trace
+trace = go.Scatter(
+    x = [range(len(dataset))],
+    y = random_y
+)
+
+data = [trace]
+
+py.iplot(data, filename='basic-line')
 
 
 
