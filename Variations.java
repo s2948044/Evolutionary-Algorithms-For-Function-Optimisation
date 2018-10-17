f3bc39797a7c387fc3e4d521386ca1e5e692570d
Kai Liang <k.liang.2@student.rug.nl>
2018-10-17 15:01:11 +0200

    some bug fixes

--- Variations.java
+++ Variations.java
@@ -55,11 +55,11 @@ public class Variations {
      * @param individual
      */
     public void nonUniformMutation(double[] individual) {
-        int counter = 0;
         for (int i = 0; i < this.cfgs.getDimension(); i++) {
+            int counter = 0;
             double temp = individual[i];
             individual[i] = individual[i] + new Random().nextGaussian() * individual[this.cfgs.getDimension()];
-            while ((individual[i] > 5 || individual[i] < -5) && counter <= 5) {
+            while ((individual[i] > 5 || individual[i] < -5) && counter < 5) {
                 counter++;
                 individual[i] = temp;
                 individual[i] = individual[i] + new Random().nextGaussian() * individual[this.cfgs.getDimension()];
@@ -102,7 +102,6 @@ public class Variations {
      * @param individual
      */
     public void singleUncorrelatedMutation(double[] individual) {
-        int counter = 0;
         individual[this.cfgs.getDimension()] = individual[this.cfgs.getDimension()]
                         * Math.exp(this.cfgs.getSingleMutationLearningRate()
                         * this.cfgs.getSingleMutationCoefficient() * new Random().nextGaussian());
@@ -110,9 +109,10 @@ public class Variations {
             individual[this.cfgs.getDimension()] = this.cfgs.getMutationStepSizeBound();
         }
         for (int i = 0; i < this.cfgs.getDimension(); i++) {
+            int counter = 0;
             double temp = individual[i];
             individual[i] = individual[i] + individual[this.cfgs.getDimension()] * new Random().nextGaussian();
-            while ((individual[i] > 5 || individual[i] < -5) && counter <= 5) {
+            while ((individual[i] > 5 || individual[i] < -5) && counter < 5) {
                 counter++;
                 individual[i] = temp;
                 individual[i] = individual[i] + individual[this.cfgs.getDimension()] * new Random().nextGaussian();
@@ -126,9 +126,9 @@ public class Variations {
      * @param individual
      */
     public void multiUncorrelatedMutation(double[] individual) {
-        int counter = 0;
         double overallNormalDist = new Random().nextGaussian();
         for (int i = 0; i < this.cfgs.getDimension(); i++) {
+            int counter = 0;
             individual[this.cfgs.getDimension() + i] = individual[this.cfgs.getDimension() + i] * Math.exp(
                     this.cfgs.getMutationLearningRate() * this.cfgs.getOverallMutationCoefficient() * overallNormalDist
                             + this.cfgs.getSecondaryMutationLearningRate() * this.cfgs.getSecondaryMutationCoefficient()
@@ -138,7 +138,7 @@ public class Variations {
             }
             double temp = individual[i];
             individual[i] = individual[i] + individual[this.cfgs.getDimension() + i] * new Random().nextGaussian();
-            while ((individual[i] > 5 || individual[i] < -5) && counter <= 5) {
+            while ((individual[i] > 5 || individual[i] < -5) && counter < 5) {
                 counter++;
                 individual[i] = temp;
                 individual[i] = individual[i] + individual[this.cfgs.getDimension() + i] * new Random().nextGaussian();
@@ -147,7 +147,6 @@ public class Variations {
     }
 
     public void correlatedMutation(double[] individual) {
-        int counter = 0;
         double[] correlationFactors = this.cfgs.getCorrelationFactors();
         double overallNormalDist = new Random().nextGaussian();
         double[] means = new double[this.cfgs.getDimension()];
@@ -187,9 +186,10 @@ public class Variations {
         double[] tmp = new double[this.cfgs.getDimension()];
         generator2.nextPoint(tmp);
         for (int i = 0; i < this.cfgs.getDimension(); i++) {
+            int counter = 0;
             double temp = individual[i];
             individual[i] = individual[i] + tmp[i];
-            while ((individual[i] > 5 || individual[i] < -5) && counter <= 5) {
+            while ((individual[i] > 5 || individual[i] < -5) && counter < 5) {
                 counter++;
                 individual[i] = temp;
                 generator2.nextPoint(tmp);
